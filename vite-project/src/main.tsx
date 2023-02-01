44fcd11fefb9b788f742496252c7d54253bcf204
姚头 <632319345@qq.com>
2023-02-01 15:03:28 +0800

    docs(*): 注释

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// 用来注入 Windi CSS 所需的样式，一定要加上！
 import 'virtual:windi.css';
 import 'virtual:svg-icons-register';
 
